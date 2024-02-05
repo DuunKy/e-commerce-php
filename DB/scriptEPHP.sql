ab4fd2aa0fecee7eaf0489bf0912ea26da115026
Liliane Dabo <lilianedabo@outlook.com>
2024-02-05 14:09:49 +0100

    first commit. Apache + MariaDB + Docker

---
+++ DB/scriptEPHP.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS phpMYSQL;
+
+CREATE DATABASE phpMYSQL;
+
+USE phpMYSQL;
+
+DROP TABLE IF EXISTS adminlogin ;
+CREATE TABLE adminlogin (uid_Admin INT AUTO_INCREMENT NOT NULL,
+username_admin VARCHAR(30),
+password_admin TEXT,
+PRIMARY KEY (uid_Admin));
